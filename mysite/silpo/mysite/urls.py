ee1010ae1d08a42007065438f8eab7a1c87c6a20
novakvova <novakvova@gmail.com>
2026-06-08 17:39:31 +0300

    begin register user

--- mysite/silpo/mysite/urls.py
+++ mysite/silpo/mysite/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
-    path('', views.homepage),
+    path('', views.homepage, name='homepage'),
 ]
--- mysite/silpo/users/views.py
+++ mysite/silpo/users/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from users.forms import CustomUserForm
+
 
 # Create your views here.
 def register(request):
-    return render(request, "register.html")
+    if request.method == 'POST':
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+
+            return redirect('homepage')
+        else:
+            messages.info(request, 'Виникли помилки при заповненні форми')
+    else:
+        form = CustomUserForm()
+
+    return render(request, "register.html", {"form": form})
