fa9fd9e7840d4a508d65280afe1aa52f1f248e0c
novakvova <novakvova@gmail.com>
2026-06-10 16:22:44 +0300

    Working app is good

--- mysite/silpo/users/forms.py
+++ mysite/silpo/users/forms.py
@@ -69,3 +69,17 @@ class CustomUserForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('email','first_name','last_name','image', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("Дана пошта уже зареєстрована")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Паролі не співпадають.")
+        return password2
--- mysite/silpo/users/views.py
+++ mysite/silpo/users/views.py
@@ -2,15 +2,30 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from users.forms import CustomUserForm
-
+from .utils import save_custom_image
+from django.contrib.auth import login
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = CustomUserForm(request.POST)
+        form = CustomUserForm(request.POST, request.FILES)
         if form.is_valid():
+            try:
+                #Обробимо форму, що заповнив користувач
+                user = form.save(commit=False)
+                if 'email' in form.changed_data:
+                    user.username = form.cleaned_data['email']
+                if 'image' in request.FILES:
+                    image = request.FILES['image']
+                    user.image_small = save_custom_image(image,size=(300,300), folder='small')
+                    user.image_medium = save_custom_image(image,size=(800,800), folder='medium')
+                    user.image_large = save_custom_image(image,size=(1200,1200), folder='large')
+                user.save()
+                login(request, user)
+                return redirect('homepage')
+            except Exception as e:
+                messages.error(request, f'Щось пішло не так: {str(e)}')
 
-            return redirect('homepage')
         else:
             messages.info(request, 'Виникли помилки при заповненні форми')
     else:
