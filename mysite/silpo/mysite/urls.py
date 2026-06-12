63cde7711a361ad66f4949664c499fc1aa2307e7
novakvova <novakvova@gmail.com>
2026-06-12 16:18:42 +0300

    Working app si good

--- mysite/silpo/mysite/urls.py
+++ mysite/silpo/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('users/', include("users.urls")),
     path('', views.homepage, name='homepage'),
 ]
+
+urlpatterns += static(settings.IMAGES_URL, document_root=settings.IMAGES_ROOT)
